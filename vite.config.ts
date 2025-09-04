634faf4f03b7056e8de0670ed35a1a15a9b0c1d6
Evgeny Yushko <yushkoevgeny@gmail.com>
2025-09-04 12:32:04 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   // Список плагинов, которые будет использовать Vite. В данном случае, только плагин для React.
   plugins: [react()],
-  // Fix: Expose process.env to the client-side code to access API_KEY.
-  define: {
-    'process.env': process.env,
-  },
 })
